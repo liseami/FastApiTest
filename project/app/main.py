6bc257585b1a15dbe59973cfea6f4e2be3fae481
liseami <51239614+liseami@users.noreply.github.com>
2021-12-31 13:16:47 +0800

    first commit

---
+++ project/app/main.py
@@ -0,0 +1,16 @@
+# project/app/main.py
+
+
+from fastapi import FastAPI,Depends
+from .config import get_settings,Settings
+
+app = FastAPI()
+
+
+@app.get("/ping")
+async def pong(settings:Settings = Depends(get_settings)):
+    return {
+        "ping": "pong!",
+        "environment": settings.environment,
+        "testing": settings.testing
+    }
