733a35f2b5229748c28274b344110d5041536a28
liseami <51239614+liseami@users.noreply.github.com>
2021-12-31 14:20:07 +0800

    first commit

--- project/app/config.py
+++ project/app/config.py
@@ -1,18 +1,23 @@
+# project/app/config.py
+
+
 import logging
 import os
+from functools import lru_cache
 
-from pydantic import BaseSettings
-from functools import  lru_cache
+from pydantic import BaseSettings, AnyUrl
+
 
 log = logging.getLogger("uvicorn")
 
 
 class Settings(BaseSettings):
-    environment:str = os.getenv("ENVIRONMENT","dev")
-    testing:bool = os.getenv("TESTING",0)
+    environment: str = os.getenv("ENVIRONMENT", "dev")
+    testing: bool = os.getenv("TESTING", 0)
+    database_url: AnyUrl = os.environ.get("DATABASE_URL")
 
 
 @lru_cache()
-def get_settings()-> BaseSettings:
-    log.info("从环境中读取配置")
+def get_settings() -> BaseSettings:
+    log.info("Loading config settings from the environment...")
     return Settings()
